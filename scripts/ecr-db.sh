c26a443d7d41686a59f347eab26ad144ae79aaff
zelaskov <marcin.zelasko@codetain.eu>
2021-12-16 17:16:06 +0100

    infra: fix typo in file name

--- scripts/ecr-db.sh
+++ scripts/ecr-db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker build ../ -t db -f ../db.Dockerfile
 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ACCOUNTID.dkr.ecr.eu-west-2.amazonaws.com
