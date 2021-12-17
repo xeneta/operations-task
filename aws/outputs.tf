db786b8e304623630b28519d46af8997cea02e3a
zelaskov <marcin.zelasko@codetain.eu>
2021-12-17 22:31:00 +0100

    infra: add terraform outputs for db host and rds master pass

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,3 +1,11 @@
 output "loadbalancer_dns" {
     value = aws_lb.task-elb.dns_name
 }
+
+output "db_host" {
+  value = aws_db_instance.db.address
+}
+
+output "rdsMasterPassword" {
+    value = nonsensitive(random_password.master_password.result)
+}
