37d40a1326f4ee2e2f238e33f1668bb5ba85cfd9
zelaskov <marcin.zelasko@codetain.eu>
2021-12-17 16:33:20 +0100

    infra: remove script for postgres db

--- scripts/ecr-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build ../ -t db -f ../db.Dockerfile
-
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ACCOUNTID.dkr.ecr.eu-west-2.amazonaws.com
-
-docker tag db $ACCOUNTID.dkr.ecr.eu-west-2.amazonaws.com/db:latest
-
-docker push $ACCOUNTID.dkr.ecr.eu-west-2.amazonaws.com/db:latest
