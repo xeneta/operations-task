d96c9ee725ec54328658ea5446092beaf6dbaa29
zelaskov <marcin.zelasko@codetain.eu>
2021-12-17 19:50:18 +0100

    infra: remove RDS hostname

--- scripts/rds.sh
+++ scripts/rds.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PGPASSWORD='password'
-PGPASSWORD=$PGPASSWORD psql -U postgres -h app-database.cme2qbt5q5x2.eu-west-2.rds.amazonaws.com -p 5432 -d rates -f ../db/rates.sql
+$HOST=""
+PGPASSWORD=$PGPASSWORD psql -U postgres -h $HOST -p 5432 -d rates -f ../db/rates.sql
