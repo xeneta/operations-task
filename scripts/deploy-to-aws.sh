c52502c03333cbf68a0c747589cea1e31da9f250
zelaskov <marcin.zelasko@codetain.eu>
2021-12-18 11:20:38 +0100

    infra: refactor bash scripts

--- scripts/deploy-to-aws.sh
+++ scripts/deploy-to-aws.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-export AWS_PAGER=less
+export AWS_PAGER=""
 
-cd ../aws && terraform init && terraform apply -auto-approve && terraform output > outputs.txt
-cd ../scripts && ./ecr-api.sh && ./rds.sh 
+cd ../aws && terraform init && terraform apply -auto-approve
+terraform output -json | jq -r '@sh "export HOST=\(.db_host.value)\nexport PGPASSWORD=\(.rds_master_password.value)\nexport ACCOUNTID=\(.account_id.value)"' > env.sh && mv env.sh ../scripts
+cd ../scripts
+chmod 777 ./env.sh && source ./env.sh
+./ecr-api.sh && ./rds.sh
 aws ecs update-service --cluster dev-cluster --service app --force-new-deployment --region eu-west-2
-cd ../aws && cat outputs.txt
+cat env.sh
--- scripts/rds.sh
+++ scripts/rds.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-PGPASSWORD=""
-$HOST=""
 PGPASSWORD=$PGPASSWORD psql -U postgres -h $HOST -p 5432 -d rates -f ../db/rates.sql
